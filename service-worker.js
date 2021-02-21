5977d0b341fbd32808759872c4e17defe8332048
imchener <imchener@users.noreply.github.com>
2021-02-21 11:53:07 +0000

    deploy: f36f150159222e7681def61e406473539b76cfc8

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@ const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
 const { ExpirationPlugin } = workbox.expiration;
 const { precacheAndRoute, matchPrecache } = workbox.precaching;
 
-precacheAndRoute([{"revision":"51b1c635de81aaf49c1b674eb91971fa","url":"favicon.ico"},{"revision":"d3a736cd3685e02e586be97515d43c1d","url":"index.html"},{"revision":"713f708b9b2662da54cd38bc98a6483f","url":"TiddlyWikiIconBlack.png"},{"revision":"2c94295d5e6cfa9e5f0b666c4ba1964c","url":"TiddlyWikiIconWhite.png"}]);
+precacheAndRoute([{"revision":"51b1c635de81aaf49c1b674eb91971fa","url":"favicon.ico"},{"revision":"6447554c9a69c46f4509f25a8f55c55a","url":"index.html"},{"revision":"713f708b9b2662da54cd38bc98a6483f","url":"tiddlywiki_icon-black.png"}]);
 
 registerRoute(
   /\.css$/,
