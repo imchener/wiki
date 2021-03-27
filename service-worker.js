ceb4faeaad6b4b7e0cb35efe7b92796533ff1e7b
imchener <imchener@users.noreply.github.com>
2021-03-27 13:46:42 +0000

    deploy: 9f4303b856e8caa9fae2fd46aa157a944f1ea396

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@ const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
 const { ExpirationPlugin } = workbox.expiration;
 const { precacheAndRoute, matchPrecache } = workbox.precaching;
 
-precacheAndRoute([{"revision":"51b1c635de81aaf49c1b674eb91971fa","url":"favicon.ico"},{"revision":"51b1c635de81aaf49c1b674eb91971fa","url":"images/favicon.ico"},{"revision":"713f708b9b2662da54cd38bc98a6483f","url":"images/tiddlywiki_icon-black.png"},{"revision":"cb8c3737317c1b2e054a2edc2a6ccb07","url":"images/wallpaper.png"},{"revision":"a2e49cc980444916393e72e3ea8a629b","url":"index.html"}]);
+precacheAndRoute([{"revision":"51b1c635de81aaf49c1b674eb91971fa","url":"favicon.ico"},{"revision":"51b1c635de81aaf49c1b674eb91971fa","url":"images/favicon.ico"},{"revision":"713f708b9b2662da54cd38bc98a6483f","url":"images/tiddlywiki_icon-black.png"},{"revision":"cb8c3737317c1b2e054a2edc2a6ccb07","url":"images/wallpaper.png"},{"revision":"572dc89dda71039eb25f7ef6b494e6be","url":"index.html"}]);
 
 registerRoute(
   /\.css$/,
